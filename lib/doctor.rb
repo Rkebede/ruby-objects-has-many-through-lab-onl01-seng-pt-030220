8ad9aef59dbd2a2d3a5813e53f737b95cc331712
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-23 21:42:53 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -24,8 +24,8 @@ class Doctor
   end 
   
   def patients
-    appointments.map do |appointment|
-      appointment.patient == patient
+    appointments.collect do |appointment|
+      appointment.patient
     end 
   end 
   
